472250f0b3824b4a5bd2e8a2eaba53bd0ddc96d4
tranhieu97 <43314948+tranhieu97@users.noreply.github.com>
2018-10-05 22:01:49 +0700

    gh

---
+++ app/src/main/java/com/itk16/bk/pc/luyentoanlop1cungbe/Menu1.java
@@ -0,0 +1,13 @@
+package com.itk16.bk.pc.luyentoanlop1cungbe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Menu1 extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+    }
+}
